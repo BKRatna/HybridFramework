e2065020c93044e69231a671a248fdc09c231323
Ratna <krishna.kotla@gmail.com>
2022-09-08 23:05:05 +0530

    Added Test Strategy BeforeClass etc

--- HybridFrameworkDesign/src/test/java/org/ratna/hybrid/TestFiles/iOS/iOSTestFile.java
+++ HybridFrameworkDesign/src/test/java/org/ratna/hybrid/TestFiles/iOS/iOSTestFile.java
@@ -4,13 +4,18 @@ import java.net.MalformedURLException;
 import java.util.HashMap;
 
 import org.ratna.hybrid.PageOjects.iOS.iOSPageObjects;
+import org.testng.annotations.BeforeMethod;
 
 import io.appium.java_client.MobileBy;
+import io.appium.java_client.android.Activity;
 //import io.appium.java_client.AppiumBy;
 import io.appium.java_client.ios.IOSDriver;
 
+//This class should extend the PageObjects class and user should use the pageobject method and pass the value here
+//@Test will come in this class. And we can group multiple tests here if the tests can be grouped. If any tests cannot be grouped then create other TestFile similarly
 public class iOSTestFile extends baseiOSTest {
 
+	IOSDriver driver;
 	public static void main(String[] args) throws MalformedURLException, InterruptedException {
 		// TODO Auto-generated method stub
 		
@@ -30,7 +35,7 @@ public class iOSTestFile extends baseiOSTest {
 //		driver.findElement(MobileBy.AccessibilityId("Confirm")).click();
 //		
 		
-		
 	}
+	
 
 }
